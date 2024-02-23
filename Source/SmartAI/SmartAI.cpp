8b4c674fd9b5f61d9f00b456235b933523b2c777
Yarik <cahloyaroslav@gmail.com>
2024-02-23 15:52:38 +0200

    delete cpp project

--- Source/SmartAI/SmartAI.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "SmartAI.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SmartAI, "SmartAI" );
-
-DEFINE_LOG_CATEGORY(LogSmartAI)
- 
