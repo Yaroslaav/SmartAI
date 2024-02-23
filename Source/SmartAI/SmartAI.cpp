d0114c0075c434360781b964439ab3cb4721afc4
Yarik <cahloyaroslav@gmail.com>
2024-02-23 14:54:41 +0200

    Add project

---
+++ Source/SmartAI/SmartAI.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SmartAI.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SmartAI, "SmartAI" );
+
+DEFINE_LOG_CATEGORY(LogSmartAI)
+ 
