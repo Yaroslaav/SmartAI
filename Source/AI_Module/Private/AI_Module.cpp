1eeab0a6bed186fcececf254646c8b6a6ddceba3
Yarik <cahloyaroslav@gmail.com>
2024-03-14 12:54:08 +0200

    Add c++

---
+++ Source/AI_Module/Private/AI_Module.cpp
@@ -0,0 +1,18 @@
+﻿#include "AI_Module.h"
+#define LOCTEXT_NAMESPACE "FAI_ModuleModule"
+
+void FAI_ModuleModule::StartupModule()
+{
+    
+}
+
+void FAI_ModuleModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FAI_ModuleModule, AI_Module)
+
+DEFINE_LOG_CATEGORY(LogAI);
---
+++ Source/AI_Module/Public/AI_Module.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Modules/ModuleManager.h"
+
+class FAI_ModuleModule : public IModuleInterface
+{
+public:
+    virtual void StartupModule() override;
+    virtual void ShutdownModule() override;
+};
+
+DECLARE_LOG_CATEGORY_EXTERN(LogAI, Log, All);
